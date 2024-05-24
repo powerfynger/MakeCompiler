106d97a60a148ca8cb44d1576b16a7aab136f3f9
powerfyng <puunika@mail.ru>
2024-05-24 16:25:02 +0300

    [feature] Добавлен вывод количества успешно обработанных строк при автотесте

--- runTests.sh
+++ runTests.sh
@@ -8,11 +8,15 @@ RED='\033[0;31m'
 
 for file in "$test_dir"/*; do
     output=$(./build/maker "$file" 2>&1)
+    total_lines=$(wc -l < "$file")
 
     if echo "$output" | grep -q "\[+\] Analysis is completed successfully."; then
-        echo -e "${GREEN}[✔] $file"
+        echo -e "${GREEN}[✔] $file $total_lines/$total_lines (100%)"
     else
-        echo -e "${RED}[✖] $file"
+        line_number=$(echo "$output" | grep -oP '(?<=\[Line )\d+(?=\])')
+        percentage=$(echo "scale=2; ($line_number / $total_lines) * 100" | bc)
+        
+        echo -e "${RED}[✖] $file $line_number/$total_lines ($percentage%)"
         # echo "$output" | grep "Line";
     fi
     # echo "-----------------------------"
