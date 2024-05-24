adac71c3de0ababc349312c13342595f568c077d
powerfyng <puunika@mail.ru>
2024-05-24 16:12:19 +0300

    [feature] Добавлены цвета в вывод информации о тестах

--- runTests.sh
+++ runTests.sh
@@ -3,14 +3,16 @@
 
 test_dir="./tests"
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
 
 for file in "$test_dir"/*; do
     output=$(./build/maker "$file" 2>&1)
 
     if echo "$output" | grep -q "\[+\] Analysis is completed successfully."; then
-        echo "[✔] $file"
+        echo -e "${GREEN}[✔] $file"
     else
-        echo "[✖] $file"
+        echo -e "${RED}[✖] $file"
         # echo "$output" | grep "Line";
     fi
     # echo "-----------------------------"
